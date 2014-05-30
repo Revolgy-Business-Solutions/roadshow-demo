77656922aec898df38d2c4d3836c3c846a6d1770
Daniel Suchy <danielsuchy87@gmail.com>
2014-05-30 16:25:59 +0200

    Add min.'s.

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 
-if [ "x$1" == "x" ] ; then
-    echo "App ID required."
-    exit
-fi
-
-appid=$1
-c="appcfg.py --oauth2 update -A $appid ."
+c="appcfg.py --oauth2 update -A roadshow-demo ."
 echo -e "\nrunning command: '$c'\n"
 $c
 echo -e "\n\ndeploy to >>$appid<< appspot instance finished.\n"
